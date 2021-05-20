deea60de496d36aecc6649ec7d76be6d07b66520
Sidhant Mohanty <84475576+sidhant-08@users.noreply.github.com>
2021-05-20 14:07:46 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,30 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      
+      <a class="navbar-brand" href="img/linkedlogo.png"><img src="img/tsflogo1.png" style="width:429px; height:40px" title="TSF Logo" alt="logo"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #001133;"><b>HOME</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #001133;"><b>CREATE USER</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #001133;"><b>TRANSFER MONEY</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #001133;"><b>TRANSACTION HISTORY</b></a>
+              </li>
+
+              <li class="nav-item">
+                <!-- <a class="nav-link" href="https://www.linkedin.com/in/sidhant-mohanty-4412741a1/" style="color : #001133;"><b>LinkedIn</b> -->
+                <!-- <img class="logo" src="img/linkedinlogo.png" alt="LinkedIn"> </a> -->
+                <a class="nav-link" href="https://www.linkedin.com/in/sidhant-mohanty-4412741a1/"><img src="img/linkedin.png" style="width:30px; height:30px" title="Linked in" alt="logo"></a>
+              </li>
+
+          </div>
+       </nav>
